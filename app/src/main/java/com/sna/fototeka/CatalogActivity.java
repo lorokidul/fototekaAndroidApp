78f2e6598b6ec76158ca716e4187e23836035519
Natalia <nataliia.sherbakova@gmail.com>
2020-05-05 18:18:45 +0300

    CatalogActivity displaing names of added files; on item click toast with file picture displayed; DocumentsHelper.fillFromFile() need fix

---
+++ app/src/main/java/com/sna/fototeka/CatalogActivity.java
@@ -0,0 +1,13 @@
+package com.sna.fototeka;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CatalogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+    }
+}
