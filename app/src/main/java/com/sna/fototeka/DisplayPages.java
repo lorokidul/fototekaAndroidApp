67ae1aed542a319576d96884d3c358f41850a884
Natalia <nataliia.sherbakova@gmail.com>
2020-05-09 00:15:56 +0300

    scale added to main page element

--- app/src/main/java/com/sna/fototeka/DisplayPages.java
+++ app/src/main/java/com/sna/fototeka/DisplayPages.java
@@ -1,12 +1,16 @@
 package com.sna.fototeka;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -16,6 +20,7 @@ import java.util.List;
 
 public class DisplayPages extends AppCompatActivity {
     private ViewPager2 viewPager2;
+    private static final int PAGE_LIMIT = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,7 +39,20 @@ public class DisplayPages extends AppCompatActivity {
         List<SliderItem> sliderItems = doc.getSliderItems();
         viewPager2.setAdapter(new SliderAdapter(sliderItems, viewPager2, DisplayPages.this));
 
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
 
-
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer(){
+            @Override
+            public void transformPage(View page, float position){
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.85f+r*0.15f);
+            }
+        });
+        viewPager2.setPageTransformer(compositePageTransformer);
     }
 }
